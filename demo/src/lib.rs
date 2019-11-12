c0d057aa8339ff451fd893327e66a5c880e37750
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-11-12 06:46:49 +0100

    demo: still trying

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -5,50 +5,50 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn handler(event: &str, _context: &str) -> String {
-    let json: Value = match from_json::<Value>(event) {
-        Ok(json) => json,
-        _ => return "{}".to_string(),
-    };
+    // let json: Value = match from_json::<Value>(event) {
+    //     Ok(json) => json,
+    //     _ => return "{}".to_string(),
+    // };
+    // 
+    // let data: &Value = match json.get("data") {
+    //     Some(data) => data,
+    //     _ => return "{}".to_string(),
+    // };
+    // 
+    // let dstr: &str = match data.as_str() {
+    //     Some(dstr) => dstr,
+    //     _ => return "{}".to_string(),
+    // };
+    // 
+    // let dbuf: Vec<u8> = match from_base64(dstr) {
+    //     Ok(dbuf) => dbuf,
+    //     _ => return "{}".to_string(),
+    // };
+    // 
+    // let original: DynamicImage = match load_image_from_memory(&dbuf) {
+    //     Ok(original) => original,
+    //     _ => return "{}".to_string(),
+    // };
+    // 
+    // let thumbnail: DynamicImage = original.thumbnail(256, 256);
 
-    let data: &Value = match json.get("data") {
-        Some(data) => data,
-        _ => return "{}".to_string(),
-    };
-
-    let dstr: &str = match data.as_str() {
-        Some(dstr) => dstr,
-        _ => return "{}".to_string(),
-    };
-
-    let dbuf: Vec<u8> = match from_base64(dstr) {
-        Ok(dbuf) => dbuf,
-        _ => return "{}".to_string(),
-    };
-
-    let original: DynamicImage = match load_image_from_memory(&dbuf) {
-        Ok(original) => original,
-        _ => return "{}".to_string(),
-    };
-
-    let thumbnail: DynamicImage = original.thumbnail(256, 256);
-
-    // let thumbnail: DynamicImage = load_image_from_memory(
-    //     &from_base64(
-    //         from_json::<Value>(event).unwrap()
-    //             .get("data").unwrap()
-    //             .as_str().unwrap(),
-    //     ).unwrap(),
-    // ).unwrap()
-    // .thumbnail(256, 256);
+    let thumbnail: DynamicImage = load_image_from_memory(
+        &from_base64(
+            from_json::<Value>(event).unwrap()
+                .get("data").unwrap()
+                .as_str().unwrap(),
+        ).unwrap(),
+    ).unwrap()
+    .thumbnail(256, 256);
 
     let mut thumbnail_buf: Vec<u8> = Vec::new();
 
-    match thumbnail.write_to(&mut thumbnail_buf, PNG) {
-        Ok(()) => (),
-        _ => return "{}".to_string(),
-    };
+    // match thumbnail.write_to(&mut thumbnail_buf, PNG) {
+    //     Ok(()) => (),
+    //     _ => return "{}".to_string(),
+    // };
 
-    // thumbnail.write_to(&mut thumbnail_buf, PNG).unwrap();
+    thumbnail.write_to(&mut thumbnail_buf, PNG).unwrap();
 
     format!("{{\"data\":\"{}\"}}", to_base64(&thumbnail_buf))
 }
---
+++ demo/test.sh
@@ -0,0 +1,1 @@
+wasmtime --disable-cache --invoke=handler $1 '{"data":"avivWN/hj/MCC2Xo9dzf0gt6TcSEUW49NFt7KfIsLfY="}' '{}' 2>&1
