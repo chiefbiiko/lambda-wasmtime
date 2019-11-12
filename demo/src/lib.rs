d867d455963bcdfb3b0c9d66479c91223b7db664
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-11-12 19:24:58 +0100

    demo: unreachable

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,37 +1,9 @@
 use base64::{decode as from_base64, encode as to_base64};
 use image::{load_from_memory as load_image_from_memory, DynamicImage, ImageOutputFormat::PNG};
 use serde_json::{from_str as from_json, Value};
-use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn handler(event: &str, _context: &str) -> String {
-    // let json: Value = match from_json::<Value>(event) {
-    //     Ok(json) => json,
-    //     _ => return "{}".to_string(),
-    // };
-    // 
-    // let data: &Value = match json.get("data") {
-    //     Some(data) => data,
-    //     _ => return "{}".to_string(),
-    // };
-    // 
-    // let dstr: &str = match data.as_str() {
-    //     Some(dstr) => dstr,
-    //     _ => return "{}".to_string(),
-    // };
-    // 
-    // let dbuf: Vec<u8> = match from_base64(dstr) {
-    //     Ok(dbuf) => dbuf,
-    //     _ => return "{}".to_string(),
-    // };
-    // 
-    // let original: DynamicImage = match load_image_from_memory(&dbuf) {
-    //     Ok(original) => original,
-    //     _ => return "{}".to_string(),
-    // };
-    // 
-    // let thumbnail: DynamicImage = original.thumbnail(256, 256);
-
+#[no_mangle]
+pub extern fn handler(event: &str, _context: &str) -> String {
     let thumbnail: DynamicImage = load_image_from_memory(
         &from_base64(
             from_json::<Value>(event).unwrap()
@@ -42,11 +14,6 @@ pub fn handler(event: &str, _context: &str) -> String {
     .thumbnail(256, 256);
 
     let mut thumbnail_buf: Vec<u8> = Vec::new();
-
-    // match thumbnail.write_to(&mut thumbnail_buf, PNG) {
-    //     Ok(()) => (),
-    //     _ => return "{}".to_string(),
-    // };
 
     thumbnail.write_to(&mut thumbnail_buf, PNG).unwrap();
 
