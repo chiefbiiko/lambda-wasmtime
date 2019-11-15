743226316be1dd1a72194948b28f7a4d5f52cfc2
Schwarz, Noah Anabiik (656) <noah_anabiik.schwarz@daimler.com>
2019-11-15 13:27:54 +0100

    impl: slim down wasm-bindgen imports

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -2,7 +2,7 @@ use anyhow::{anyhow, Result as AnyhowResult};
 use base64::{decode as from_base64, encode as to_base64};
 use image::{load_from_memory as load_image_from_memory, ImageOutputFormat::PNG};
 use serde_json::{from_str as from_json, Value};
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 fn process(event: &str, mut thumbnail_buf: Vec<u8>) -> AnyhowResult<Vec<u8>> {
     load_image_from_memory(
