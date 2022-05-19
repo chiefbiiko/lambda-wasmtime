bf2f73ad43587a6ed57160438b0752e872387878
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2022-05-19 15:57:45 +0200

    chore: use wit to generate code

--- demo/demo.sh
+++ demo/demo.sh
@@ -14,7 +14,7 @@ EOL
 
 cd "${1:-"$(pwd)"}"
 
-wasm=./target/wasm32-wasi/release/demo.wasm
+wasm=../target/wasm32-wasi/release/handler.wasm
 event="{\"data\":\"$(base64 ./luigi.png)\"}"
 context="{}"
 
@@ -22,20 +22,20 @@ context="{}"
 cargo wasi build --release
 
 # run an export from our wasm module - passing in strings!!!
-result="$(wasmtime --disable-cache --enable-simd --invoke=handler "$wasm" "$event" "$context" 2>&1 | grep -v warning)"
+# ../bin/wasmtime --invoke=handler "$wasm" "$event" "$context"
 
 # massage the image from JSON to a PNG
-node -e "fs.writeFileSync('./thumbnail.png',Buffer.from(JSON.parse('$result').data,'base64'))"
+# node -e "fs.writeFileSync('./thumbnail.png',Buffer.from(JSON.parse('$result').data,'base64'))"
 
 # inspect images
-if ! viu -ntv ./luigi.png ./thumbnail.png; then
-  cargo install viu
-  viu -ntv ./luigi.png ./thumbnail.png
-fi
+# if ! viu -ntv ./luigi.png ./thumbnail.png; then
+#   cargo install viu
+#   viu -ntv ./luigi.png ./thumbnail.png
+# fi
 
 # zipup a demo lambda bundle
 # can be deployed on aws with the runtime layer - get its latest release from:
 # https://github.com/chiefbiiko/lambda-wasmtime/releases/latest
 # make sure to provision the lambda with approx. 2048 MB memory and a 5s timeout
 # note: all of this is mvp and experimental at the moment
-zip -j ./demo.zip "$wasm" > /dev/null
+zip -j ./handler.zip "$wasm" > /dev/null
--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,69 +1,34 @@
-use std::{convert::From, ffi::CString, mem, os::raw::c_char};
 use bytes::Bytes;
-use http::{Method, request::Builder};
+use http::{request::Builder, Method};
 use serde_json::{from_str as from_json, Value};
 use wasi_experimental_http::request;
 
-#[no_mangle]
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn _start(ptr: *mut u8, size: usize) -> *mut c_char {
-    let event_str;
-    unsafe {
-        let data = Vec::from_raw_parts(ptr, size, size);
-        // read a Rust `String` from the byte array,
-        event_str = String::from_utf8(data).unwrap();
+use handler::*;
+wit_bindgen_rust::export!("../handler.wit");
+
+struct Handler {}
+impl handler::Handler for Handler {
+    fn handler(event: Event, context: Option<Context>) -> Result<Output, Error> {
+        let json = from_json::<Value>(event.as_str()).unwrap();
+        println!("{:?} {:?}", json, context);
+        let url = "https://postman-echo.com/post".to_string();
+        let req = Builder::new()
+            .method(Method::POST)
+            .uri(&url)
+            .header("Content-Type", "text/plain")
+            .header("abc", "def");
+        let b = Bytes::from("Testing");
+        let req = req.body(Some(b)).unwrap();
+        println!("{:?}", req);
+
+        let mut res = request(req).expect("cannot make request");
+        let str = std::str::from_utf8(&res.body_read_all().unwrap())
+            .unwrap()
+            .to_owned();
+        println!("{:?}", str);
+        println!("{:#?}", res.header_get("content-type".to_string()).unwrap());
+        let status_code = res.status_code;
+        println!("{:#?}", status_code);
+        Ok(str)
     }
-    let json = from_json::<Value>(event_str.as_str()).unwrap();
-    println!("{:?}", json);
-    let url = "https://postman-echo.com/post".to_string();
-    let req = Builder::new()
-        .method(Method::POST)
-        .uri(&url)
-        .header("Content-Type", "text/plain")
-        .header("abc", "def");
-    let b = Bytes::from("Testing");
-    let req = req.body(Some(b)).unwrap();
-    println!("{:?}", req);
-
-    let mut res = request(req).expect("cannot make request");
-    // println!("{:?}", res.into());
-    let str = std::str::from_utf8(&res.body_read_all().unwrap()).unwrap().to_owned();
-    println!("{:?}", str);
-    println!("{:#?}", res.header_get("content-type".to_string()).unwrap());
-    let status_code = res.status_code;
-    println!("{:#?}", status_code);
-    // let input_str = "whatever".to_string();
-    let s = CString::new(str).unwrap();
-    let ptr = s.as_ptr();
-    println!("{:?}", ptr);
-    // mem::forget(s);
-    s.into_raw()
-    // Tuple {
-    //     ptr,
-    //     size,
-    // }
-}
-
-/// Allocate memory into the module's linear memory
-/// and return the offset to the start of the block.
-#[no_mangle]
-pub fn alloc(size: usize) -> *mut u8 {
-    // create a new mutable buffer with capacity `len`
-    let mut buf = Vec::with_capacity(size);
-    // take a mutable pointer to the buffer
-    let ptr = buf.as_mut_ptr();
-    // take ownership of the memory block and
-    // ensure the its destructor is not
-    // called when the object goes out of scope
-    // at the end of the function
-    mem::forget(buf);
-    // return the pointer so the runtime
-    // can write data at this offset
-    ptr
-}
-
-#[no_mangle]
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub fn dealloc(ptr: *mut c_char) {
-    let _ = unsafe { CString::from_raw(ptr) };
 }
